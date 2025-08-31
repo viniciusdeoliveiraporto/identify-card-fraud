c3916d3abaaf8a4153c3b082cca3911a4ce61181
viniciusdeoliveiraporto <vinicius.oliveira.porto@ccc.ufcg.edu.br>
2025-08-31 13:57:24 -0300

    feat: add autoencoder

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -7,15 +7,18 @@ def split_train_test():
 
     df = df_verdadeiro.head(543)
 
-    df_normal = df[df["Class"] == 0]
-    df_fraud = df[df["Class"] == 1]
+    df_normal = df[df['Class'] == 0]
+    df_fraud = df[df['Class'] == 1]
 
     df_train, df_val = train_test_split(df_normal, test_size=0.2, random_state=42)
 
     df_test = pd.concat([df_val, df_fraud])
-    labels_test = pd.concat([df["Class"][df_val.index], df["Class"][df_fraud.index]])
 
-    return df_train, df_test, labels_test
+    ds_train = df_train.drop(columns=['Class']).values  
+    ds_test = df_test.drop(columns=['Class']).values  
+    labels_test = df_test['Class'].values  
+
+    return ds_train, ds_test, labels_test
 
 
 if __name__ == "__main__":
